74f90d251b91f4ea52cd86d0affee93f265e5e8e
D4rk Gh057 <efuelpaz8870@uta.edu.ec>
2023-07-13 09:57:35 -0500

    se modifica el sql

--- proyecto.sql
+++ proyecto.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 09-07-2023 a las 04:00:02
+-- Tiempo de generación: 13-07-2023 a las 16:56:51
 -- Versión del servidor: 10.4.28-MariaDB
--- Versión de PHP: 8.1.17
+-- Versión de PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,6 +24,26 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `categoria`
+--
+
+CREATE TABLE `categoria` (
+  `id_cat` varchar(4) NOT NULL,
+  `nom_cat` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `categoria`
+--
+
+INSERT INTO `categoria` (`id_cat`, `nom_cat`) VALUES
+('E01', 'ELECTRONICOS'),
+('L01', 'LIBROS'),
+('UE01', 'UTILES ESCOLARES');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `clientes`
 --
 
@@ -31,18 +51,42 @@ CREATE TABLE `clientes` (
   `cedula` char(10) NOT NULL,
   `nombre` varchar(50) NOT NULL,
   `apellido` varchar(50) NOT NULL,
-  `edad` int(11) NOT NULL,
   `telefono` varchar(50) NOT NULL,
   `correo` varchar(100) NOT NULL,
-  `contraseña` varchar(100) NOT NULL
+  `direccion` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `clientes`
 --
 
-INSERT INTO `clientes` (`cedula`, `nombre`, `apellido`, `edad`, `telefono`, `correo`, `contraseña`) VALUES
-('0950134395', 'Bryan', 'Obando', 22, '0983231570', 'bryanobandochavez@gmail.com', 'visual123');
+INSERT INTO `clientes` (`cedula`, `nombre`, `apellido`, `telefono`, `correo`, `direccion`) VALUES
+('0950134395', 'Bryan', 'Obando', '0983231570', 'bryanobandochavez@gmail.com', 'UTA'),
+('1801020304', 'Stalin', 'Badillo', '0987654321', 'badilleins@gmail.com', 'UTA'),
+('1850178870', 'Daniel', 'Fuelpaz', '0983201541', 'erikfuelpaz@gmail.com', 'Av.Atahualpa y Rio Salado');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empleados`
+--
+
+CREATE TABLE `empleados` (
+  `cedula` varchar(10) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `apellido` varchar(50) NOT NULL,
+  `edad` int(11) NOT NULL,
+  `telefono` varchar(100) NOT NULL,
+  `correo` varchar(100) NOT NULL,
+  `contraseña` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `empleados`
+--
+
+INSERT INTO `empleados` (`cedula`, `nombre`, `apellido`, `edad`, `telefono`, `correo`, `contraseña`) VALUES
+('0950134395', 'Bryan', 'Obando', 21, '0983231570', 'bryanobandochavez@gmail.com', 'visual123');
 
 -- --------------------------------------------------------
 
@@ -51,15 +95,36 @@ INSERT INTO `clientes` (`cedula`, `nombre`, `apellido`, `edad`, `telefono`, `cor
 --
 
 CREATE TABLE `productos` (
-  `id` int(11) NOT NULL,
+  `id` varchar(20) NOT NULL,
   `nombre` varchar(50) NOT NULL,
   `precio` double NOT NULL,
-  `categoria` varchar(50) NOT NULL
+  `categoria` varchar(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `productos`
+--
+
+INSERT INTO `productos` (`id`, `nombre`, `precio`, `categoria`) VALUES
+('6939119035198', 'HAVIT TW938 True Wireless Gaming Earbuds', 45.5, 'E01'),
+('6957895955486', 'GRAPAS PANDI YLS003', 0.59, 'UE01'),
+('7861123900737', 'PEGAMENTO BARRA BESTER 36GR', 0.5, 'UE01'),
+('7891360472629', 'MINAS FABER CASTELL 0.5MM HB', 0.4, 'UE01'),
+('9788479533380', 'QUIEN SE HA LLEVADO MI QUESO?', 2.9, 'L01'),
+('9788499084978', 'CONTROLE SU DESTINO', 12, 'L01'),
+('9789584258748', 'Un Mundo Lleno De Futuro', 21.8, 'L01'),
+('9789942202581', 'INVESTIGACION OPERATIVA', 10, 'L01'),
+('9789978160893', 'LA MEJOR EDAD PARA MORIR', 5, 'L01');
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`id_cat`);
 
 --
 -- Indices de la tabla `clientes`
@@ -68,20 +133,27 @@ ALTER TABLE `clientes`
   ADD PRIMARY KEY (`cedula`);
 
 --
+-- Indices de la tabla `empleados`
+--
+ALTER TABLE `empleados`
+  ADD PRIMARY KEY (`cedula`);
+
+--
 -- Indices de la tabla `productos`
 --
 ALTER TABLE `productos`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `categoria` (`categoria`);
 
 --
--- AUTO_INCREMENT de las tablas volcadas
+-- Restricciones para tablas volcadas
 --
 
 --
--- AUTO_INCREMENT de la tabla `productos`
+-- Filtros para la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  ADD CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`categoria`) REFERENCES `categoria` (`id_cat`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
